bc72517443836881762e0a038118e0490d112731
Dan <dgworkmail123@gmail.com>
2019-12-03 23:54:47 +0100

    Änderungen Startseite 0412 - Hovereffekt Button, Routing Startseite

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { StartButtonsComponent } from './start-buttons/start-buttons.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: '', component: StartButtonsComponent},
+  {path: '**', component: StartButtonsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
