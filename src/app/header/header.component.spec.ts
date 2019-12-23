afba4d141e3c38e984fa8bfd3413b8590a1c52b3
Dan <dgworkmail123@gmail.com>
2019-12-23 12:35:03 +0100

    first tests

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,25 +1,70 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { HeaderData } from '../headerData.model';
+import { Component } from '@angular/core';
+// import { element, by } from 'protractor';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+  let headerComponent: HeaderComponent;
+  let headerFixture: ComponentFixture<HeaderComponent>;
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent, TestHostComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    headerFixture = TestBed.createComponent(HeaderComponent);
+    headerComponent = headerFixture.componentInstance;
+    headerFixture.detectChanges();
+
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
+    testHostFixture.detectChanges();
   });
 
+
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(headerComponent).toBeTruthy();
   });
+
+
+  it('should create headerComponent with Input', async(() => {
+     // provide inputs here
+     headerComponent.objHeaderData = new HeaderData("Inventarverwaltung", "Startseite","Hallo Administrator, was möchten Sie tun?");
+    expect(headerComponent).toBeTruthy();
+  }));
+
+
+  it('should have a title with "Inventarverwaltung"', () => {
+
+    // testHostComponent.setObjHeaderData("Inventarverwaltung", "Startseite","Hallo Administrator, was möchten Sie tun?");
+    // testHostFixture.detectChanges();
+    // expect(testHostFixture.debugElement.nativeElement.querySelector('app-title').innerText).toEqual('Inventarverwaltung');
+    // Cannot read property 'innerText' of null
+
+    const appTitle = headerFixture.debugElement.nativeElement.querySelector('app-title');
+    expect(appTitle.getText()).toBe('Inventarverwaltung');
+  });
+
+
+  @Component({
+      selector: 'host-component',
+      template: '<app-header [objHeaderData]="objHeaderData"></app-header>'
+    })
+    class TestHostComponent {
+      private objHeaderData: HeaderData;
+
+      setObjHeaderData(strTitle:string, strPath:string, strMessage:string) {
+        this.objHeaderData = new HeaderData(strTitle, strPath, strMessage);
+      }
+  }
+
+
 });
