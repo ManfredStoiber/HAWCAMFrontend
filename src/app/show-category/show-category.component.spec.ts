2513e2c9a0b70a6ee6e9a8f0d913e2f0506ed6c2
Manfred Stoiber <ManfredStoiber@gmail.com>
2020-01-12 23:47:38 +0100

    improved unittests

--- src/app/show-category/show-category.component.spec.ts
+++ src/app/show-category/show-category.component.spec.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 
 
 import { ShowCategoryComponent } from './show-category.component';
+import { DataService } from '../data.service';
 
 describe('ShowCategoryComponent', () => {
   let component: ShowCategoryComponent;
@@ -15,7 +16,7 @@ describe('ShowCategoryComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ ShowCategoryComponent ],
       imports: [ReactiveFormsModule ],
-      providers: [{provide: Router, useClass: RouterStub}]
+      providers: [{provide: Router, useClass: RouterStub}, {provide: DataService, useClass: DataServiceMock}]
     })
     .compileComponents();
   }));
@@ -36,6 +37,87 @@ describe('ShowCategoryComponent', () => {
     expect(routerNavigateSpy).toHaveBeenCalledWith(["/editCategory"]);
   });
 });
+
+
+describe('ShowCategoryComponent with no values', () => {
+  let component: ShowCategoryComponent;
+  let fixture: ComponentFixture<ShowCategoryComponent>;
+
+  let alertSpy;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShowCategoryComponent ],
+      imports: [ReactiveFormsModule],
+      providers: [{provide: Router, useClass: RouterStub}, {provide: Location, useClass: LocationStub}, {provide: DataService, useClass: DataServiceMock}]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowCategoryComponent);
+    component = fixture.componentInstance;
+
+    alertSpy = spyOn(window, "alert");
+    spyOn(TestBed.get(DataService), "getJsonCatAttributes");
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(alertSpy).toHaveBeenCalledWith("Fehler bei der Dateiübertragung, bitte Seite erneut mit Auswahl laden");
+  });
+
+});
+
+describe('ShowCategoryComponent with error', () => {
+  let component: ShowCategoryComponent;
+  let fixture: ComponentFixture<ShowCategoryComponent>;
+
+  let alertSpy;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShowCategoryComponent ],
+      imports: [ReactiveFormsModule],
+      providers: [{provide: Router, useClass: RouterStub}, {provide: Location, useClass: LocationStub}, {provide: DataService, useClass: DataServiceMock}]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowCategoryComponent);
+    component = fixture.componentInstance;
+
+    alertSpy = spyOn(window, "alert");
+    spyOn(TestBed.get(DataService), "getJsonCatAttributes").and.returnValue({"Fehler": "Testfehler"});
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(alertSpy).toHaveBeenCalledWith("Fehler bei der Dateiübertragung, bitte Seite erneut mit Auswahl laden");
+  });
+
+});
+
+class DataServiceMock {
+  getJsonCatAttributes() {
+    return {"attributes": [
+      {"mandatory": "0"},
+      {"mandatory": "0", "typ": "dateAndTime", "name": "Testdetail 1"},
+      {"mandatory": "1"},
+      {"mandatory": "1", "typ": "dateAndTime", "name": "Testdetail 2"},
+      {"mandatory": "x"},
+    ]};
+  }
+}
+
+class LocationStub {
+  back() {}
+}
 
 class RouterStub {
   navigate(destination: String) {}
--- src/app/show-object/show-object.component.spec.ts
+++ src/app/show-object/show-object.component.spec.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { ShowObjectComponent } from './show-object.component';
+import { DataService } from '../data.service';
 
 describe('ShowObjectComponent', () => {
   let component: ShowObjectComponent;
@@ -14,7 +15,7 @@ describe('ShowObjectComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ ShowObjectComponent ],
       imports: [ ReactiveFormsModule, HttpClientModule ],
-      providers: [{provide: Router, useClass: RouterStub}]
+      providers: [{provide: Router, useClass: RouterStub}, {provide: DataService, useClass: DataServiceMock}]
     })
     .compileComponents();
   }));
@@ -36,6 +37,86 @@ describe('ShowObjectComponent', () => {
   });
 });
 
+describe('ShowObjectComponent with no values', () => {
+  let component: ShowObjectComponent;
+  let fixture: ComponentFixture<ShowObjectComponent>;
+
+  let alertSpy;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShowObjectComponent ],
+      imports: [ReactiveFormsModule, HttpClientModule],
+      providers: [{provide: Router, useClass: RouterStub}, {provide: Location, useClass: LocationStub}, {provide: DataService, useClass: DataServiceMock}]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowObjectComponent);
+    component = fixture.componentInstance;
+
+    alertSpy = spyOn(window, "alert");
+    spyOn(TestBed.get(DataService), "getObjectDetails");
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(alertSpy).toHaveBeenCalledWith("Fehler bei der Dateiübertragung, bitte Seite erneut mit Auswahl laden");
+  });
+
+});
+
+describe('ShowObjectComponent with error', () => {
+  let component: ShowObjectComponent;
+  let fixture: ComponentFixture<ShowObjectComponent>;
+
+  let alertSpy;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShowObjectComponent ],
+      imports: [ReactiveFormsModule, HttpClientModule],
+      providers: [{provide: Router, useClass: RouterStub}, {provide: Location, useClass: LocationStub}, {provide: DataService, useClass: DataServiceMock}]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowObjectComponent);
+    component = fixture.componentInstance;
+
+    alertSpy = spyOn(window, "alert");
+    spyOn(TestBed.get(DataService), "getObjectDetails").and.returnValue({"Fehler": "Testfehler"});
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(alertSpy).toHaveBeenCalledWith("Fehler bei der Dateiübertragung, bitte Seite erneut mit Auswahl laden");
+  });
+
+});
+
+class LocationStub {
+  back() {}
+}
+
 class RouterStub {
   navigate(destination: String) {}
+}
+
+class DataServiceMock {
+  getObjectDetails() {
+    return {"details": [
+      {"mandatory": "0"},
+      {"mandatory": "0", "typ": "dateAndTime", "name": "Testdetail 1"},
+      {"mandatory": "1"},
+      {"mandatory": "1", "typ": "dateAndTime", "name": "Testdetail 2"},
+      {"mandatory": "x"},
+    ]};
+  }
 }
