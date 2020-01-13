ca4db39332e82fe830cf26885d7320ad920b2f3b
Dan <dgworkmail123@gmail.com>
2020-01-13 01:10:35 +0100

    changes to rest.spec

--- src/app/rest.service.spec.ts
+++ src/app/rest.service.spec.ts
@@ -1,14 +1,108 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RESTService } from './rest.service';
 
+
+
 describe('RESTService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule]
-  }));
+
+  let checkAlertSpy;
+  let service: RESTService;
+  let httpMock: HttpTestingController;
+
+  beforeEach( async( () => TestBed.configureTestingModule({
+    imports:
+      [
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+    providers: [ RESTService ]
+
+  })));
+
+  beforeEach(() => {
+    checkAlertSpy = spyOn(window, 'alert').and.callThrough();
+    service = TestBed.get(RESTService);
+    httpMock = TestBed.get(HttpTestingController);
+  })
+
+
+  afterEach( () => {
+    httpMock.verify();  // waits until none requests are outstanding
+  })
+
 
   it('should be created', () => {
-    const service: RESTService = TestBed.get(RESTService);
     expect(service).toBeTruthy();
   });
+
+
+  it('should have a GET that returns a Observable<JSON>', () => {
+
+    const dummyResponse: any =
+    {
+      categories:
+        [
+          {name: "Tee"},
+          {name: "Flaschen"},
+          {namr: "Raum"}
+        ]
+    }
+
+    service.getFromRESTService('test').subscribe( response => {
+      expect(response).toBe(dummyResponse);
+    });
+
+    const request = httpMock.expectOne( service.strUrl + 'test');
+
+    expect( request.request.method).toBe('GET');
+    request.flush(dummyResponse);
+
+  });
+
+
+  // it('should have a POST that returns a Observable<JSON>', () => {
+  //
+  //   const jsonToBackend: JSON = JSON.parse('{"search": "' + "'" + "Raum" + "'" + '"}');
+  //
+  //   const dummyResponse: any =
+  //   {
+  //   categories:
+  //       [
+  //           {name: "Raum"}
+  //       ],
+  //       objects:
+  //       [
+  //           {name: "Raum1", cat:"Raum"},
+  //           {name: "Raum2", cat:"Raum"}
+  //       ]
+  //   };
+  //
+  //   service.putToRESTService('test', jsonToBackend).subscribe( response => {
+  //     expect(response).toBe(dummyResponse);
+  //   });
+  //
+  //   const request = httpMock.expectOne( service.strUrl + 'test');
+  //
+  //   expect( request.request.method).toBe('PUT');
+  //   request.flush(dummyResponse);
+  //
+  // });
+
+
+
+  it('should alert user on error', () => {
+
+    let myError: any = {
+      error: new Error('This is going horribly wrong'),
+      message:"testmessage",
+      status:300
+    }
+    service.handleError( myError );
+    expect(window.alert).toHaveBeenCalledWith("Fehlgeschlagen, Info siehe Console");
+
+  });
+
+
 });
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -12,7 +12,7 @@ export class RESTService {
 
   // membervariables
   private nPort: number = 5001;
-  private strUrl: string = "http://snirps.ddns.net:" + this.nPort + "/api/v1.0/";
+  strUrl: string = "http://snirps.ddns.net:" + this.nPort + "/api/v1.0/";
 
   private headerDict : any;
 
@@ -67,7 +67,7 @@ export class RESTService {
   // handleError - method to handle errors: ParseErrors, HttpErrors...
   // @param   error   error from calling backendservice
   // @return          errormessage
-  private handleError( error: any) {
+  public handleError( error: any) {
 
     let strErrorMessage = '';
     if (error.error instanceof ErrorEvent) {
@@ -85,68 +85,5 @@ export class RESTService {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-  // processing response
-  private processingResponse( strResponse: string ) : JSON {
-
-    console.log("--processingResponse");
-
-    let valid : boolean = this.validateResponse(strResponse);
-    if( valid == true ){
-          console.log("yes yes yes");
-          console.log("mach was");
-          return this.json;
-    } else {
-        console.log("abbruch");
-        this.json = null;
-        return JSON.parse( '{"Fehler":"fehlerhaftes JSON aus Backend"}' )
-    }
-
-  }
-
-
-  // method for validating the response
-  // check whether the returned JSON is valid
-  private validateResponse( strResponse: string ) : boolean {
-
-    console.log("--validateResponse");
-    console.log("------------");
-    console.log("Response wäre:");
-    console.log(strResponse);
-    console.log("------------");
-
-    try {
-        // kann er nicht wegen dem vorangestellten Test
-        // das ist schon ein JSON - kann er nicht nochmal da rein hauen
-    // this.json = JSON.parse( 'Test {"name":"bob","age":34}' );
-
-      // kann er
-      this.json = JSON.parse( '[ {"id": 1, "name": "Leanne Graham", "username": "Bret" } ]' );
-      // this.json = JSON.parse( strResponse );
-
-      console.log("--validateResponse nach JSON.parse");
-      console.log(this.json)
-      return true;
-
-    } catch ( exception ) {
-      console.log("no no no");
-      console.log("errorroutine here");
-    }
-    return false;
-  }
-
-*/
 
 }
