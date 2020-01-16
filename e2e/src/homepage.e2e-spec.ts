6d63a193422714af107f9f1d939194363a425056
Manfred Stoiber <ManfredStoiber@gmail.com>
2020-01-16 12:05:12 +0100

    improved e2e-test

--- e2e/src/homepage.e2e-spec.ts
+++ e2e/src/homepage.e2e-spec.ts
@@ -2,8 +2,15 @@ import {by, element, browser} from 'protractor';
 describe('HAWCAM homepage', function () {
 	it('should show the HAWCAM homepage', function() {
 		
+		// go to homepage
 		browser.get('http://snirps.ddns.net/SE-Project/DEV/Inventarverwaltung');
 
+		// check if all buttons are present
 		expect(element(by.id('app-message')).getText()).toMatch('Hallo.*, was möchten Sie tun?');
+		expect(element(by.id('bttnCreateObject').isPresent()).toBe(true);
+		expect(element(by.id('bttnSearch').isPresent()).toBe(true);
+		expect(element(by.id('bttnCreateCategory').isPresent()).toBe(true);
+		expect(element(by.id('bttnListCategories').isPresent()).toBe(true);
+
 	});
 });
